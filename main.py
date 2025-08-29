188219e1d7cce5c7875cefce793795a018435951
kid126987 <kid126987@gmail.com>
2025-08-29 15:28:48 +0800

    Update main.py
    刪除sidepro

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 if __name__ == "__main__":
-   from sidepro.database_setup import *
+   from database_setup import *
    db_path = check_and_create_database()
 
-   from sidepro.DataCraptoDB import *
+   from DataCraptoDB import *
    DatatoDBS = DatatoDB()
 
-   from sidepro.gradio_app import *
+   from gradio_app import *
+
    demo.launch()
